521a60945bf08a8a9a5ea33b13c9d686f7811dbf
mdb <mdb@protonmail.ch>
2017-03-02 13:46:48 +0800

    Added menu and Faq contentw

--- public/main.go
+++ public/main.go
@@ -5,13 +5,19 @@ import (
 	ttemplate "text/template"
 )
 
-var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("index.html"))
-
 func init() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/faq", faq)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("index.html"))
+	htmlTempl.Execute(w, "")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("faq.html"))
 	htmlTempl.Execute(w, "")
 }
