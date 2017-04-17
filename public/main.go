7ce30a0211d2b9df74a50352a9bb3f33b2cef088
mdb <mdb@protonmail.ch>
2017-04-17 23:54:23 +0800

    Contenr updates

--- public/main.go
+++ public/main.go
@@ -8,6 +8,8 @@ import (
 func init() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/faq", faq)
+	http.HandleFunc("/my-approach", myApproach)
+	http.HandleFunc("/therapy-for-adolescents", therapyForAdolescents)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -19,5 +21,17 @@ func root(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("faq.html"))
+	htmlTempl.Execute(w, "")
+}
+
+func myApproach(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("my-approach.html"))
+	htmlTempl.Execute(w, "")
+}
+
+func therapyForAdolescents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	var htmlTempl = ttemplate.Must(ttemplate.ParseFiles("therapy-for-adolescents.html"))
 	htmlTempl.Execute(w, "")
 }
